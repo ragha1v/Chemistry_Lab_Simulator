cc79300a46382c3c3d8eee31f6df626cbf5abbd3
ragha1v <106020992+ragha1v@users.noreply.github.com>
2023-02-03 00:24:54 +0530

    Add files via upload

---
+++ LogicChemistry.java
@@ -0,0 +1,26 @@
+package Lec54;
+import java.util.*;
+public class LogicChemistry {
+	public static void main(String [] args) {
+		Scanner sc=new Scanner(System.in);
+		//N1V1=N2V2
+		String Element=sc.next();
+		System.out.println(Element);
+		double n1=sc.nextDouble();
+		if(n1<=5 || n1>=0.01)
+		{
+			double v1=sc.nextDouble();
+			double minmoles=0.01;
+			double maxmoles=5;
+			float n2= (float) (Math.random() * (maxmoles-minmoles+1)+minmoles);
+			System.out.println(n2);
+			double v2=0;
+			while(n1*v1!=(int)(n2*v2))
+			{
+				System.out.println((int)(n2*v2));
+				v2+=0.0001;
+			}
+			System.out.println(v2);
+		}
+	}
+}
