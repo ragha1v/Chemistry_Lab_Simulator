7c13b14fb3659b43b48116aba6383358493d1812
Rahul Khandelwal <rahulcoolkhandelwal123@gmail.com>
2023-02-20 00:02:36 +0530

    Add files via upload

---
+++ SecondFrame.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+public class SecondFrame {
+	private JFrame f = new JFrame("Chemistry Workstation");
+	
+	public SecondFrame() {
+	
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setSize(595,460);
+		f.setVisible(true);
+	}
+}
